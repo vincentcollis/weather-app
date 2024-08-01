0a1903a9aaa8727f49200bf7a0cd623909e16502
Vincent Collis <vince@Vincents-Mac-Studio.local>
2024-07-31 19:40:20 -0500

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,52 @@
 import React, { useEffect, useState } from "react";
-import LocationBox from './components/LocationBox'
-import './App.css';
-// import "../components.css"
+import LocationBox from './components/LocationBox';
 
-function App (props){
-    const [locationInputBoxState ,setlocationInputBoxState]= useState('')
-    
-    const [appBackgroundState, setappBackgroundState] = useState('')
+function App(props) {
+    const [locationInputBoxState, setlocationInputBoxState] = useState('');
+    const [appBackgroundState, setappBackgroundState] = useState('');
 
-
-    function backgroundHandler(locationBoxInput){
-        setlocationInputBoxState(locationBoxInput)
+    function backgroundHandler(locationBoxInput) {
+        setlocationInputBoxState(locationBoxInput);
     }
 
-    async function getData(URL){
-        try{
-            const response = await fetch(URL)
-            if(!response.ok){
-                return console.log('Not valid zipcode')
+    async function getData(URL) {
+        try {
+            const response = await fetch(URL);
+            if (!response.ok) {
+                return console.log('Not valid zipcode');
             }
-            const data = await response.json()
-            console.log(data)
+            const data = await response.json();
+            console.log(data);
 
-            if(data.temperature > 80){
-                console.log("its hot")
-                setappBackgroundState('blue')
+            if (data.temperature > 80) {
+                console.log("its hot");
+                setappBackgroundState('blue');
             }
-        } catch(error) {
-            console.log(error)
+        } catch (error) {
+            console.log(error);
         }
     }
 
-    // run function depending on state change of LocationBox
-    useEffect(()=>{
-        const ZIP = locationInputBoxState
-        const URL = `http://localhost:3000/api/?zip=${ZIP}`
+    useEffect(() => {
+        const ZIP = locationInputBoxState;
+        const URL = `http://localhost:3000/api/?zip=${ZIP}`;
 
-        // if users input box has 5 charecters, send fetch request
-        if(locationInputBoxState.length === 5){
-            getData(URL)
+        if (locationInputBoxState.length === 5) {
+            getData(URL);
         }
-    },[locationInputBoxState])
+    }, [locationInputBoxState]);
 
-    return(
-        <div id="root">
-            <LocationBox 
-            id={appBackgroundState}
-            backgroundHandler={backgroundHandler} 
-            locationBoxInput = {locationInputBoxState} />
+    return (
+        <div id="mainContainer">
+            <img id="logo" src="./public/weather.svg" style={{ width: "200px" }} alt="Weather Logo" />
+            <span id="inputBoxHeader">Easy Weather</span>
+            <LocationBox
+                id={appBackgroundState}
+                backgroundHandler={backgroundHandler}
+                locationBoxInput={locationInputBoxState}
+            />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/LocationBox.jsx
+++ src/components/LocationBox.jsx
@@ -1,35 +1,32 @@
 import React, { useEffect } from "react";
-import {useState} from "react"
+import { useState } from "react";
+import "../index.css";
+import "../App.css";
 
-import '../App.css';
+function LocationBox(props) {
+  const backgroundHandler = props.backgroundHandler;
+  const locationBoxInput = props.locationBoxInput;
 
-function LocationBox (props){
-    const backgroundHandler = props.backgroundHandler
-    const locationBoxInput = props.locationBoxInput
+  function inputHandler(e) {
+    e.preventDefault();
+    const input = e.target.value;
+    backgroundHandler(input);
+  }
 
-    // const [zipCodeState,setZipCodeState] = useState('')
-
-    function inputHandler(e){
-        e.preventDefault()
-        
-        const input = e.target.value
-        backgroundHandler(input)
-    }
-
-    console.log(props.locationBoxInput)
-    
-    return(
-            <form>
-                <label>
-                    Location: 
-                <input 
-                    className="locationBoxInputBox" 
-                    value={locationBoxInput}
-                    onChange={ e => inputHandler(e)}
-                ></input>
-                </label>
-            </form>
-    )
+  return (
+    <form>
+      <label>
+        <div>
+          <input
+            placeholder="ZIPCODE"
+            className="locationBoxInputBox "
+            value={locationBoxInput}
+            onChange={(e) => inputHandler(e)}
+          />
+        </div>
+      </label>
+    </form>
+  );
 }
 
-export default LocationBox
+export default LocationBox;
